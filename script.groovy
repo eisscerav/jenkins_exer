32d7f63c640b21d49e2b95e600832e373b543854
eisscerav <eisscerav@163.com>
2022-04-20 21:05:29 +0800

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -2,4 +2,14 @@ def build_app() {
   echo 'build app function'
 }
 
+def test_app() {
+  echo 'test app function'
+  echo "use parameter ${params.choice_var}"
+}
+
+def deploy_app() {
+  echo 'build app function'
+  echo "use parameter ${params.string_var}"
+}
+
 return this
