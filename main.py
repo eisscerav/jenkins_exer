0a02a1d0518b5e332a452e801702863680cc6883
ffan <ffan@nvidia.com>
2022-05-05 20:58:52 +0800

    add argparse

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+import argparse
+
+
 def demo():
     print("demo python")
 
 
-if __name__ == '__main__':
-    demo()
+def demo_argparse():
+    parser = argparse.ArgumentParser(description='name')
+    parser.add_argument('--family', type=str, help='first name')
+    parser.add_argument('--name', type=str, dest='ffan_name', metavar='meta', help='last name')
+    args = parser.parse_args()
 
+                        print(f"args.family={args.family}, args.ffan_name={args.ffan_name}")
+if __name__ == '__main__':
+#    demo()
+    demo_argparse()
