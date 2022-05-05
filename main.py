7b91974138ab3fb212274a57112cb7257c398d3d
ffan <ffan@nvidia.com>
2022-05-05 21:00:17 +0800

    format argparse

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ def demo_argparse():
     parser.add_argument('--family', type=str, help='first name')
     parser.add_argument('--name', type=str, dest='ffan_name', metavar='meta', help='last name')
     args = parser.parse_args()
+    print(f"args.family={args.family}, args.ffan_name={args.ffan_name}")
 
-                        print(f"args.family={args.family}, args.ffan_name={args.ffan_name}")
+
 if __name__ == '__main__':
 #    demo()
     demo_argparse()
