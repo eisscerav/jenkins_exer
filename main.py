99df1043ebf6d80a82c0c41caa1e52e2bb213e98
eisscerav <eisscerav@163.com>
2022-11-08 14:33:51 +0800

    print envs

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import os
+
+
 def demo():
     print("demo python")
 
 
 if __name__ == '__main__':
+    envs = os.environ
+    for env in envs:
+        print(f'key={env}, value={envs[env]}')
     demo()
 
